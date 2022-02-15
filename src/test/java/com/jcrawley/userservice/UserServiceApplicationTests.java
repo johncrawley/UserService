b376203e22fd85f28ecb40a5e2e6ede5fe732922
johncrawley <johncrawley1@gmail.com>
2022-02-15 11:23:52 +0000

    Initial Commit

---
+++ src/test/java/com/jcrawley/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
